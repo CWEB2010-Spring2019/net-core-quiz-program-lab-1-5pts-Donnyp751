b0857e6916bcf1680e8ed0ba7af361a2415da846
Donald Posterick <donny.p@live.com>
2019-01-10 11:59:00 -0600

    added project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("DotNetCoreQuiz");
+
+            
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
